26fdc10f27b093ec8058dc59dbf490ff6ea7b24f
Alex Babkin <ababkin@sumall.com>
2015-04-17 15:07:18 -0400

    add more stuff: opaque SemVer type, some helpers

--- src/Network/Eureka/SemVer.hs
+++ src/Network/Eureka/SemVer.hs
@@ -1,12 +1,30 @@
 module Network.Eureka.SemVer (
-    addSemVer
+    SemVer
+  , addSemVer
+  , parseSemVer
   ) where
 
-import           Data.Version   (Version, showVersion)
-import           Network.Eureka (InstanceConfig (..), addMetadata)
+import Data.Maybe                   (listToMaybe)
+import Data.Version                 (Version, parseVersion, showVersion)
+import Network.Eureka               (InstanceConfig (..), addMetadata)
+import Text.ParserCombinators.ReadP (readP_to_S)
+
+type SemVer = Version
+
+metadataSemVerKey = "semver"
+
+showSemVer :: SemVer -> String
+showSemVer = showVersion
+
+parseSemVer :: String -> Maybe SemVer
+parseSemVer =
+  fmap fst
+  . listToMaybe
+  . filter (null . snd)
+  . readP_to_S parseVersion
 
 addSemVer
   :: Version
   -> InstanceConfig
   -> InstanceConfig
-addSemVer semver = addMetadata ("semver", showVersion semver)
+addSemVer semver = addMetadata (metadataSemVerKey, showVersion semver)
